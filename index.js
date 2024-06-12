816f52306c6fac0754e1574b4c933270bffcde14
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-06-11 23:49:02 -0600

    Update

--- index.js
+++ index.js
@@ -12,17 +12,63 @@ function getMenuHtml(){
                 <h2 class="price">$${menuItem.price}</h2>
             </div>
             <div class="btn-container">
-                <button class="add-btn" id="add-btn">+</button>
+                <button class="add-btn" id="${menuItem.name}-btn">+</button>
             </div>
+        
         </div>
         <hr>
         `
     })
+
     return menuHtml
 }
 
-function renderOptions(){
-    document.getElementById('menu').innerHTML = getMenuHtml()
+
+function buttonFunction(){
+    const addBtns = document.querySelectorAll('.add-btn')
+    addBtns.forEach(function(button){
+        button.addEventListener("click", function(){
+            const item = button.getAttribute('id')
+            if(item === 'Pizza-btn'){
+                addToCart('pizza', 14)
+                renderTotal(14)
+            }
+            else if(item ==='Hamburger-btn'){
+                addToCart('Hamburger', 12)
+                renderTotal(12)
+            }
+            else if(item ==='Beer-btn'){
+                addToCart('Beer', 7)
+                renderTotal(7)
+            }
+        })
+    })
 }
 
-renderOptions()
+
+function renderMenu(){
+    document.getElementById('menu').innerHTML = getMenuHtml()
+    buttonFunction()
+}
+
+
+function addToCart(item, price){
+    totalPrice += price
+    let cart = document.getElementById('cart')
+    cart.innerHTML += `
+        <p>${item} costs ${price}</p>
+    `
+}
+
+let totalPrice = 0
+function renderTotal(cost){
+    totalPrice += cost
+    let finalCost = document.getElementById('total')
+    finalCost.innerHTML = `
+        <p> Total Price = ${totalPrice}</p>
+    `
+}
+
+
+renderMenu()
+
